8c48379255368bf2e3c81d6dee30fd7470dda93b
codersoumya23 <63958041+codersoumya23@users.noreply.github.com>
2020-09-16 20:28:21 +0530

    Update receiver_code.ino

--- Alpha-14/receiver_code/receiver_code.ino
+++ Alpha-14/receiver_code/receiver_code.ino
@@ -4,11 +4,11 @@
 // with a simple ASK transmitter in a very simple way.
 // Implements a simplex (one-way) receiver with an Rx-B1 module
 
-#include <RH_ASK.h>
-#include <SPI.h> // Not actualy used but needed to compile
+#include <RH_ASK.h> //includes the RadioHead lib
+#include <SPI.h> 
 
 
-RH_ASK driver;
+RH_ASK driver;   //object driver called
 
 void setup()
 {
@@ -16,19 +16,17 @@ void setup()
     if (!driver.init())
          Serial.println("init failed");
 }
-
+/*In the loop() a buffer is set whose size is equal to the size of the data that will be received
+*/
 void loop()
 {
     uint8_t buf[30];
     uint8_t buflen = sizeof(buf);
 
-    if (driver.recv(buf, &buflen)) // Non-blocking
+    if (driver.recv(buf, &buflen)) // checking whether the data is valid or not
     {
 	int i;
-
-	// Message with a good checksum received, dump it.
-	//driver.printBuffer("Got:", buf, buflen);
-  Serial.print("Data:");
-  Serial.println((char*)buf);
+        Serial.print("Data:");          //printing the received data
+        Serial.println((char*)buf);
     }
 }
